ae46ee61cb08a25d770bc7649306491ecdb7f800
David G. Paul <jedi58@users.noreply.github.com>
2015-10-29 08:21:01 +0000

    Update bootstrap.php
    Corrected phpunit bootstrap

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 error_reporting(E_ALL);
 
-require __DIR__ . '/../../bootstrap.php';
-
 $autoloader = require __DIR__ . '/../../vendor/autoload.php';
 $autoloader->add('Inachis\\Tests\\', __DIR__);
