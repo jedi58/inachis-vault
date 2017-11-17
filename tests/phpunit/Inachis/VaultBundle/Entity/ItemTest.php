c11aa8c4ea664d0b1280444bfe84512caf8bafbd
David G. Paul <jedi58@gmail.com>
2017-11-17 11:10:45 +0000

    Fixed namespaces

--- tests/phpunit/Inachis/VaultBundle/Entity/ItemTest.php
+++ tests/phpunit/Inachis/VaultBundle/Entity/ItemTest.php
@@ -1,14 +1,18 @@
 <?php
+namespace Inachis\Tests\VaultBundle\Entity;
+
+use Inachis\Component\VaultBundle\Entity\Item;
+
 /**
  * @group unit
  */
-class ItemTest extends PHPUnit_Framework_TestCase
+class ItemTest extends \PHPUnit_Framework_TestCase
 {
     protected $item;
     
     public function __construct($name = null, array $data = array(), $dataName = '')
     {
-        $this->item = new Inachis\Vault\Item();
+        $this->item = new Item();
         //parent::__construct($name, $data, $dataName);
     }
    
